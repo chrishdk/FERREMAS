9c0445944c1fabee1d5bfd9be568286681d98777
ItaloNatann <italo.natann.r@gmail.com>
2024-06-05 21:39:52 -0400

    integration_test

---
+++ ferremas/apps/products/tests/integration/test_products.py
@@ -0,0 +1,28 @@
+import pytest
+from rest_framework.test import APIClient
+from django.urls import reverse
+from rest_framework import status
+
+@pytest.mark.django_db
+class TestAccountAPI:
+    def test_user_products_successfully(self):
+        """
+        Test para verificar que el endpoint de registro de productos funciona correctamente.
+        """
+        client = APIClient()
+
+        url = reverse('add-to-cart')
+
+        data = {
+            'name': 'taladro',
+            'description': 'taladrito',
+            'price': '111'
+        }
+
+        response = client.post(url, data, format='json')
+
+        assert response.status_code == status.HTTP_201_CREATED
+
+        assert 'name' in response.data
+
+        assert response.data['name'] == 'taladro'
